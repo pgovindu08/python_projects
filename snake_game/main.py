ef1d71842bb7ef4ce0b85605d3e94e039ecf2579
pgovindu08 <pssg2103@gmail.com>
2026-01-04 17:04:59 -0600

    snake game

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,4 +1,6 @@
 from turtle import Screen, Turtle
+from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -6,25 +8,32 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.tracer(n=0, delay=0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
+snake = Snake()
+food = Food()
 
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+score = 0
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for seg in segments:
-        seg.forward(20)
-        screen.update()
-        time.sleep(1)
+    snake.move()
 
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        score += 1
+        snake.extend()
+
+    if snake.hit_wall():
+        game_is_on = False
+        print("Game Over")
+        print(f"Score: {score}")
 
 screen.title("My Snake Game")
 screen.exitonclick()
